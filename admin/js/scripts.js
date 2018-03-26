f8ccebf16e7aae5b11e8ed8630d54bc29e8dc8ba
Rob <robert.saternus@gmail.com>
2018-03-26 18:31:31 +0200

    Add form of local events page

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -13,3 +13,22 @@
     });
     
 })(jQuery);
+
+
+function makeFileList() {
+    var input = document.getElementById("filesToUpload");
+    var ul = document.getElementById("fileList");
+    while (ul.hasChildNodes()) {
+        ul.removeChild(ul.firstChild);
+    }
+    for (var i = 0; i < input.files.length; i++) {
+        var li = document.createElement("li");
+        li.innerHTML = input.files[i].name;
+        ul.appendChild(li);
+    }
+    if(!ul.hasChildNodes()) {
+        var li = document.createElement("li");
+        li.innerHTML = 'Nie wybrano żadnego pliku';
+        ul.appendChild(li);
+    }
+}
